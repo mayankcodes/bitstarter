560a67d9ce8ddcfb2fc626974f875f9bf59f0fed
Ubuntu <ubuntu@ip-172-31-23-204.us-west-2.compute.internal>
2013-07-26 02:19:20 +0000

    new setup file

---
+++ setup.sh
@@ -0,0 +1,11 @@
+sudo apt-get install -y git-core;wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh;
+sudo apt-get update;
+sudo apt-get install -y python-software-properties python g++ make;
+sudo add-apt-repository ppa:chris-lea/node.js;
+sudo apt-get update;
+sudo apt-get install -y nodejs;
+npm install restler csv accounting;
+npm install cheerio;
+npm install commander;
+npm install express;
+
